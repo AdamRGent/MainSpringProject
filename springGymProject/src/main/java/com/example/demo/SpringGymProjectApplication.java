65b02f705a463cf9f33372eac3dfc8513f810f4d
AdamRGent <adrgent09@hotmail.com>
2022-03-17 09:57:02 +0000

    updated version

---
+++ springGymProject/src/main/java/com/example/demo/SpringGymProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGymProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGymProjectApplication.class, args);
+	}
+
+}
